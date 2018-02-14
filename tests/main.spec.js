99e2af99d65cb4ac2c52e873c4b808b9a8be5bc4
gjlacerda <guilherme.lcd@gmail.com>
2018-02-14 21:46:45 -0200

    Initializing CLI and tests

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,5 +1,15 @@
-import { expect } from 'chai';
+const expect = require('chai').expect;
+const exec = require('child_process').exec;
+const btcConverter = './src/main.js';
 
-describe('Main', () => {
-
+describe('main CLI', () => {
+    it('should return hello world', (done) => {
+        exec(btcConverter, (err, stdout, stderr) => {
+            if (err) {
+                throw err;
+            }
+            expect(stdout.replace('\n', '')).to.be.equal('hello world!');
+            done();
+        });
+    });
 });
