711fbea009a182e235a361c72e8d31a3fd489b89
gjlacerda <guilherme.lcd@gmail.com>
2018-02-20 21:38:09 -0300

    Requested API to get real data

--- tests/convertBTC.spec.js
+++ tests/convertBTC.spec.js
@@ -1,13 +1,62 @@
 const chai = require('chai');
 const expect = chai.expect;
 const convertBTC = require('../src/convertBTC');
+const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
+const nock = require('nock');
+
+chai.use(sinonChai);
 
 describe('convertBTC', () => {
-    it('should return USD as currency and 1 as amount default', () => {
-        expect(convertBTC()).to.be.equal('1 BTC to USD = 2000.00');
+    let consoleStub;
+    
+    const responseMock = {
+        "price": 22213.54,
+        "success": true,
+        "time": "2018-02-21 00:06:34"
+    };
+
+    beforeEach(() => {
+        consoleStub = sinon.stub(console, 'log');
     });
 
-    it('should return BRL as currency and 10 as amount when defined', () => {
-        expect(convertBTC('BRL', 10)).to.be.equal('10 BTC to BRL = 2000.00');
+    afterEach(() => {
+        consoleStub.restore();
+    });
+    
+    it('should use currency USD and 1 as amount default', (done) => {
+        nock('https://apiv2.bitcoinaverage.com')
+            .get('/convert/global')
+            .query({
+                from: 'BTC',
+                to: 'USD',
+                amount: 1
+            })
+            .reply(200, responseMock)
+
+        convertBTC();
+
+        setTimeout(() => {
+            expect(consoleStub).to.have.been.calledWith('1 BTC to USD = 22213.54');
+            done();
+        }, 300);
+    });
+
+    it('should use currency USD and 10 as amount default', (done) => {
+        nock('https://apiv2.bitcoinaverage.com')
+            .get('/convert/global')
+            .query({
+                from: 'BTC',
+                to: 'USD',
+                amount: 10
+            })
+            .reply(200, responseMock)
+
+        convertBTC('USD', 10);
+
+        setTimeout(() => {
+            expect(consoleStub).to.have.been.calledWith('10 BTC to USD = 22213.54');
+            done();
+        }, 300);
     });
 });
