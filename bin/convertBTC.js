b31732d479a6bf7b3146e3fb226ee38510d0a9a8
gjlacerda <guilherme.lcd@gmail.com>
2018-02-22 23:52:27 -0300

    Added loader

--- bin/convertBTC.js
+++ bin/convertBTC.js
@@ -2,12 +2,21 @@
 
 var request = require('request');
 var chalk = require('chalk');
+var ora = require('ora');
+
+var spinner = ora({
+    text: 'Retrieving Bitcoin data...',
+    color: 'yellow'
+});
 
 function convertBTC() {
     var currency = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'USD';
     var amount = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 1;
 
     var url = 'https://apiv2.bitcoinaverage.com/convert/global?from=BTC&to=' + currency.toUpperCase() + '&amount=' + amount;
+
+    spinner.start();
+
     request(url, function (error, request, body) {
         var response = void 0;
         try {
@@ -16,6 +25,8 @@ function convertBTC() {
             console.log(chalk.red('Something wrong.'));
             return error;
         }
+
+        spinner.stop();
 
         console.log(chalk.red(amount) + ' BTC to ' + chalk.cyan(currency.toUpperCase()) + ' = ' + chalk.yellow(response.price));
     });
--- src/convertBTC.js
+++ src/convertBTC.js
@@ -1,8 +1,17 @@
 const request = require('request');
 const chalk = require('chalk');
+const ora = require('ora');
+
+const spinner = ora({
+    text: 'Retrieving Bitcoin data...',
+    color: 'yellow'
+});
 
 function convertBTC(currency = 'USD', amount = 1) {
     const url = `https://apiv2.bitcoinaverage.com/convert/global?from=BTC&to=${currency.toUpperCase()}&amount=${amount}`;
+
+    spinner.start();
+
     request(url, (error, request, body) => {
         let response;
         try {
@@ -12,6 +21,8 @@ function convertBTC(currency = 'USD', amount = 1) {
             console.log(chalk.red('Something wrong.'));    
             return error;
         }
+
+        spinner.stop();
 
         console.log(`${chalk.red(amount)} BTC to ${chalk.cyan(currency.toUpperCase())} = ${chalk.yellow(response.price)}`);
     });
